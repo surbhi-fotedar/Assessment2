ddc4ba89737cb2c8f2f4ca7d0dfb897b07840039
Vinoth J <vinoth.jayaprakash@cesltd.com>
2018-08-20 16:13:24 +0530

    Created an optimum implememntation for displaying date & day information Added a reference to the new js file in html file

---
+++ js/display-date-info-optimal.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  function getDayOfWeek(value) {
+    if (value >= 0 && value < 7) {
+      console.log(daysOfWeek[value]);
+      return daysOfWeek[value];
+    } else {
+      console.log('The day is not valid');
+    }
+  }
+
+  function findSundayDate(value, dayNumber) {
+    try {
+      return value.setDate(value.getDate() - dayNumber);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function displayDateInfo(value) {
+    try {
+      value = new Date(value);
+      var d = value.getDay();
+
+      if (d === 0) {
+        console.log(value + 'is a Sunday');
+        return;
+      } else {
+        console.log(value + ' is ' + getDayOfWeek(d) + ' and the corresponding Sunday date is ' + findSundayDate(value, d));
+      }
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  displayDateInfo('August 29, 2018');
+
+})();
