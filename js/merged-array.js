7202704b32c76bbf14ebe6285c63ef9119823295
Vinoth J <vinoth.jayaprakash@cesltd.com>
2018-08-20 16:11:07 +0530

    Added a code review comment

--- js/merged-array.js
+++ js/merged-array.js
@@ -3,7 +3,7 @@
   var arr = [2, 3, 4, 5, 6, 7];
 
   function mergedArray(arr) {
-    var newArr = arr.concat(arr);
+    var newArr = arr.concat(arr); // Parameter must be checked for the correct type before using concat
     console.log(newArr);
   }
 
